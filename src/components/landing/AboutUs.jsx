12114b98544ca6382a7682eef5e62f3e0fb24314
SunilPdl <mrsunilpaudel@gmail.com>
2023-07-05 12:23:36 +0545

    page: about page add

---
+++ src/components/landing/AboutUs.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div className="container aboutUs-section">
+      <h2>About Us</h2>
+      <div className="flex">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti,
+          ullam eum rerum nobis laboriosam quia nulla recusandae repudiandae
+          impedit soluta magnam. Corporis quod molestiae impedit ipsa veritatis
+          est? Voluptates, molestias.
+        </p>
+        <img src="/aboutUs.svg" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
