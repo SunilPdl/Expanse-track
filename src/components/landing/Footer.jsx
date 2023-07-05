7adaab0f9faf6e2da085141b28573383fda2fbd5
SunilPdl <mrsunilpaudel@gmail.com>
2023-07-05 12:24:10 +0545

    page: footer page add

---
+++ src/components/landing/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container" pppppppppppppp>
+        <div className="flex">
+          <span>
+            <img src="/vite.svg" alt="footer" />
+          </span>
+          <ul>
+            <li>Fulbari,pokhara</li>
+            <li>+977 9876543210</li>
+            <li>thankadigital@gmail.com</li>
+          </ul>
+        </div>
+        <hr />
+        <p>2023&copy; all rights reserved</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
