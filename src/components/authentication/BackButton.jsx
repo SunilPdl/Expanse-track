51bd572c99a22aa290874ebeeaed5429e1a54f62
SunilPdl <mrsunilpaudel@gmail.com>
2023-07-06 13:46:28 +0545

    add: add back button

---
+++ src/components/authentication/BackButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ArrowLeftCircle } from 'react-feather'
+import { Link } from 'react-router-dom'
+
+const BackButton = () => {
+  return (
+    <Link to='/'>
+      <span className='back-btn'>
+      <ArrowLeftCircle />
+    </span>
+    </Link>
+  )
+}
+
+export default BackButton
