96290bf2991a90926cbbd0d15d34230a5be689fc
SunilPdl <mrsunilpaudel@gmail.com>
2023-07-06 13:52:31 +0545

    add: add services page

--- src/components/landing/Services.jsx
+++ src/components/landing/Services.jsx
@@ -1,22 +1,27 @@
 import React from "react";
+import ServiceCard from "./components/ServiceCard";
+import { Airplay, BarChart, Bell } from "react-feather";
 
 const Services = () => {
   return (
-    <div className="container">
+    <div className="container" id="service">
       <h2>Services</h2>
       <div className="flex services-list">
-        <div>
-          <img src="/easy.svg" alt="" />
-          <p>Easy to use</p>
-        </div>
-        <div>
-          <img src="/report.svg" alt="" />
-          <p>Monthly report</p>
-        </div>
-        <div>
-          <img src="/userFriendly.svg" alt="" />
-          <p>User friendly</p>
-        </div>
+        <ServiceCard
+          icon={<Airplay />}
+          title="USER FRIENDLY"
+          desp="Expense Tracking is user friendly & very easy to use."
+        />
+        <ServiceCard
+          icon={<BarChart />}
+          title="REPORTING"
+          desp="Expense Tracking provide you with weekly reporting."
+        />
+        <ServiceCard
+          icon={<Bell />}
+          title="NOTIFICATION"
+          desp="Expense Tracking provide you with timely notifications."
+        />
       </div>
     </div>
   );
